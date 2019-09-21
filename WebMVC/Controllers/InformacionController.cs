1cfc2a576eaf40b7ea916468c8e360e4c358434d
Alan <Alan@DESKTOP-M949GPQ>
2019-09-21 12:08:48 -0500

    Agregar archivos de proyecto.

---
+++ WebMVC/Controllers/InformacionController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class InformacionController : Controller
+    {
+        // GET: Informacion
+        public ActionResult VistaAngular()
+        {
+            return View();
+        }
+
+        public JsonResult CargarTabla()
+        {
+            List<DatosTabla> listaDatos = new List<Models.DatosTabla>();
+            DatosTabla datos = new Models.DatosTabla();
+
+            datos.Id = 1;
+            datos.Nombre = "Nombre 1";
+            datos.Descripcion = "Descripcion 1";
+            listaDatos.Add(datos);
+
+            datos = new Models.DatosTabla();
+            datos.Id = 2;
+            datos.Nombre = "Nombre 2";
+            datos.Descripcion = "Descripcion 2";
+            listaDatos.Add(datos);
+
+            datos = new Models.DatosTabla();
+            datos.Id = 3;
+            datos.Nombre = "Nombre 3";
+            datos.Descripcion = "Descripcion 3";
+            listaDatos.Add(datos);
+
+            return Json(listaDatos, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AgregarRegistroTabla()
+        {
+            return Json(new DatosTabla(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AgregarRegistroTabla2(List<DatosTabla> ListaDatos)
+        {
+            DatosTabla datosTabla = new DatosTabla();
+            datosTabla.Id = 5;
+            datosTabla.Nombre = "Nombre 5";
+            datosTabla.Descripcion = "Descripcion 5";
+            ListaDatos.Add(datosTabla);
+
+            return Json(ListaDatos, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ WebMVC/Scripts/Informacion/Informacion.js
@@ -0,0 +1,102 @@
+﻿var app = angular.module("MyApp", []);
+
+app.controller("MyController", function ($scope, $http, $window) {
+
+    $scope.HolaMundo = "Hola mundo 2.";
+
+    $scope.Boton = "Da click";
+
+    $scope.MostrarBoton = true;
+
+    $scope.ControlText = "Escribe algo";
+
+    $scope.ClickBoton = function () {
+        $scope.HolaMundo = "Ya realizaste click.";
+    }
+
+    $scope.DobleClickBoton = function (texto, texto2) {
+        $scope.HolaMundo = "Ya realizaste doble click.";
+
+        $scope.Boton = texto + "-" + texto2;
+    }
+
+    $scope.LlamarControlador = function()
+    {
+        $.ajax({
+            type: "POST",
+            url: "/Informacion/CargarTabla",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (datos) {
+
+                $scope.ListaDatos = datos;
+
+                $scope.$apply();
+
+            },
+            error: function (error) {
+
+            }
+        });
+    }
+
+    $scope.ModificarColumna = function(tablaDatos)
+    {
+        tablaDatos.Nombre = tablaDatos.Nombre + "-Nuevo";
+
+        $scope.$apply();
+    }
+
+    $scope.AgregarElemento = function (tablaDatos) {
+
+        $.ajax({
+            type: "POST",
+            url: "/Informacion/AgregarRegistroTabla",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (datos) {
+
+                datos.Id = 4;
+                datos.Nombre = "Nombre 1A";
+                datos.Descripcion = "Descripcion 1A";
+
+                $scope.ListaDatos.push(datos);
+
+                $scope.$apply();
+
+            },
+            error: function (error) {
+
+            }
+        });
+
+        //$scope.$apply();
+    }
+
+    $scope.AgregarElemento2 = function () {
+
+        $.ajax({
+            type: "POST",
+            url: "/Informacion/AgregarRegistroTabla2",
+            data: JSON.stringify(
+                {
+                    'ListaDatos': $scope.ListaDatos
+                }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (datos) {
+
+                $scope.ListaDatos = datos;
+
+                $scope.$apply();
+
+            },
+            error: function (error) {
+
+            }
+        });
+
+
+    }
+
+});
